3162f1649f2e4271e54d45baf53ef2a746224c56
Andy Luu <luuandyf2nd@gmail.com>
2022-09-04 15:57:41 -0700

    Time: 211 ms, Memory: 54.6 MB - LeetHub

--- product-of-array-except-self/product-of-array-except-self.js
+++ product-of-array-except-self/product-of-array-except-self.js
@@ -2,74 +2,24 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-// var productExceptSelf = function(nums) {
-//     const answerArray = new Array(nums.length);
-//     const leftArrProducts = new Array(nums.length);
-//     const rightArrProducts = new Array(nums.length);
-//     leftArrProducts[0] = 1;
-//     rightArrProducts[nums.length - 1] = 1;
-    
-//     for (let i = 1; i < nums.length; i++) {
-//         leftArrProducts[i] = nums[i - 1] * leftArrProducts[i - 1];
-//     }
-    
-//     for (let x = nums.length - 2; x >= 0; x--) {
-//         rightArrProducts[x] = nums[x + 1] * rightArrProducts[x + 1];
-//     }
-    
-
-//     for (let j = 0; j < answerArray.length; j++) {
-//         answerArray[j] = leftArrProducts[j] * rightArrProducts[j];
-//     }
-    
-//     return answerArray;
-    
-    
-// };
-
-// Input: nums = [1,2,3,4]
-
-// Output: [24,12,8,6]
-
-// [1, 1, 2, 6]
-
 var productExceptSelf = function(nums) {
-    const answerArr = new Array(nums.length);
-    answerArr[0] = 1;
+    
+    let answerArray = new Array(nums.length);
+    
+    answerArray[0] = 1;
     
     for (let i = 1; i < nums.length; i++) {
-        answerArr[i] = nums[i - 1] * answerArr[i - 1]; 
+        answerArray[i] = nums[i - 1] * answerArray[i - 1];
     }
     
-    let currProduct = 1;
+    let rightProduct = 1;
+    
     for (let x = nums.length - 1; x >= 0; x--) {
-        answerArr[x] = answerArr[x] * currProduct;
-        currProduct *= nums[x];
+        
+        answerArray[x] = answerArray[x] * rightProduct;
+        
+        rightProduct *= nums[x];
     }
     
-    return answerArr;
+    return answerArray;
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
