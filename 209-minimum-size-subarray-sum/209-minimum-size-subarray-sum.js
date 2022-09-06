d019d865dfaa345b913589260b85080d933cb8d9
Andy Luu <luuandyf2nd@gmail.com>
2022-09-05 17:21:30 -0700

    Time: 128 ms (16.59%), Space: 45.8 MB (88.64%) - LeetHub

---
+++ 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number} target
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minSubArrayLen = function(target, nums) {
+    let windowStart = 0;
+    let windowSum = 0;
+    let minLength = Infinity;
+    
+    for (let windowEnd = 0; windowEnd < nums.length; windowEnd++) {
+        windowSum += nums[windowEnd];
+        
+        while (windowSum >= target) {
+            minLength = Math.min(minLength, windowEnd - windowStart + 1);
+            windowSum -= nums[windowStart];
+            windowStart++;
+        }
+    }
+    
+    if (minLength === Infinity) {
+        return 0;
+    }
+    
+    return minLength;
+};
