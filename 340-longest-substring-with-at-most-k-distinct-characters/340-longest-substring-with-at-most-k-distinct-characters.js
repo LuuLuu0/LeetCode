b0819327adec0334d7bfc4a0611825a1e9ac40d1
Andy Luu <luuandyf2nd@gmail.com>
2022-09-05 18:00:57 -0700

    Time: 185 ms (29.41%), Space: 48.3 MB (22.91%) - LeetHub

--- 340-longest-substring-with-at-most-k-distinct-characters/340-longest-substring-with-at-most-k-distinct-characters.js
+++ 340-longest-substring-with-at-most-k-distinct-characters/340-longest-substring-with-at-most-k-distinct-characters.js
@@ -4,29 +4,30 @@
  * @return {number}
  */
 var lengthOfLongestSubstringKDistinct = function(s, k) {
-  let windowStart = 0,
-    maxLength = 0,
-    charFrequency = {};
-
-  // in the following loop we'll try to extend the range [window_start, window_end]
-  for (let windowEnd = 0; windowEnd < s.length; windowEnd++) {
-    const rightChar = s[windowEnd];
-    if (!(rightChar in charFrequency)) {
-      charFrequency[rightChar] = 0;
+    let windowStart = 0;
+    let charMap = {};
+    let maxLength = -Infinity;
+    
+    for (let windowEnd = 0; windowEnd < s.length; windowEnd++) {
+        const rightChar = s[windowEnd];
+        
+        if (!charMap[rightChar]) {
+            charMap[rightChar] = 0;
+        }
+        charMap[rightChar] += 1;
+        
+        while (Object.keys(charMap).length > k) {
+            let leftChar = s[windowStart];
+            charMap[leftChar] -= 1;
+            
+            if (charMap[leftChar] === 0) {
+                delete charMap[leftChar];
+            }
+            windowStart += 1;
+        }
+        
+        maxLength = Math.max(maxLength, windowEnd - windowStart + 1);
     }
-    charFrequency[rightChar] += 1;
-    // shrink the sliding window, until we are left with 'k' distinct characters in the char_frequency
-    while (Object.keys(charFrequency).length > k) {
-      const leftChar = s[windowStart];
-      charFrequency[leftChar] -= 1;
-      if (charFrequency[leftChar] === 0) {
-        delete charFrequency[leftChar];
-      }
-      windowStart += 1; // shrink the window
-    }
-    // remember the maximum length so far
-    maxLength = Math.max(maxLength, windowEnd - windowStart + 1);
-  }
-
-  return maxLength;
+    
+    return maxLength;
 };
