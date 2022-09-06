62a0d7e373bfcc4686d2eb5a2dc3a3d512760201
Andy Luu <luuandyf2nd@gmail.com>
2022-09-06 11:46:42 -0700

    Time: 205 ms (40.60%), Space: 48.1 MB (47.86%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.js
+++ 567-permutation-in-string/567-permutation-in-string.js
@@ -5,38 +5,34 @@
  */
 var checkInclusion = function(s1, s2) {
     let windowStart = 0;
-    let charMap = {};
-    let matchedChars = 0;
+    let stringMap = {};
+    let matchCount = 0;
     
     for (let letter of s1) {
-        if (!(letter in charMap)) charMap[letter] = 0;
-        charMap[letter] += 1;
+        if (!(letter in stringMap)) stringMap[letter] = 0;
+        stringMap[letter] += 1;
     }
     
     for (let windowEnd = 0; windowEnd < s2.length; windowEnd++) {
-        const currLetter = s2[windowEnd];
-
-        if (currLetter in charMap) {
-            charMap[currLetter] -= 1;
-            if (charMap[currLetter] === 0) {
-                matchedChars += 1;
-            }
-        }
+        let leftLetter = s2[windowEnd];
         
-        if (matchedChars === Object.keys(charMap).length) return true;
+        if (leftLetter in stringMap) {
+            stringMap[leftLetter] -= 1;
+            if (stringMap[leftLetter] === 0) matchCount += 1;
+        }
+        if (matchCount === Object.keys(stringMap).length) return true;
         
         if (windowEnd >= s1.length - 1) {
-            let prevChar = s2[windowStart];
+            let rightLetter = s2[windowStart];
             windowStart += 1;
             
-            if (prevChar in charMap) {
-                if (charMap[prevChar] === 0) {
-                    matchedChars -= 1;
+            if (rightLetter in stringMap) {
+                if (stringMap[rightLetter] === 0) {
+                    matchCount -= 1;
                 }
-                charMap[prevChar] += 1;
+                stringMap[rightLetter] += 1;
             }
         }
     }
-    
     return false;
 };
