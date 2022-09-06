cb60a93cdc90c651489dd204f0d296593ae67a6e
Andy Luu <luuandyf2nd@gmail.com>
2022-09-06 11:30:13 -0700

    Time: 215 ms (38.92%), Space: 47.9 MB (48.86%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.js
+++ 567-permutation-in-string/567-permutation-in-string.js
@@ -5,39 +5,38 @@
  */
 var checkInclusion = function(s1, s2) {
     let windowStart = 0;
-    let windowEnd = s1.length;
-    let patternMap = rebuildMap(s1);
+    let charMap = {};
+    let matchedChars = 0;
     
-    console.log(patternMap)
-    while(windowEnd <= s2.length) {
-        let currentString = s2.substring(windowStart, windowEnd);
-        
-        for (let letter of currentString) {
-            patternMap[letter] -= 1;
-            if (patternMap[letter] === 0) {
-                delete patternMap[letter];
+    for (let letter of s1) {
+        if (!(letter in charMap)) charMap[letter] = 0;
+        charMap[letter] += 1;
+    }
+    
+    for (let windowEnd = 0; windowEnd < s2.length; windowEnd++) {
+        const currLetter = s2[windowEnd];
+
+        if (currLetter in charMap) {
+            charMap[currLetter] -= 1;
+            if (charMap[currLetter] === 0) {
+                matchedChars += 1;
             }
         }
         
-        if (Object.keys(patternMap).length === 0) {
-            return true;
-        }
+        if (matchedChars === Object.keys(charMap).length) return true;
         
-        windowStart += 1;
-        windowEnd += 1;
-        patternMap = rebuildMap(s1);
+        if (windowEnd >= s1.length - 1) {
+            let prevChar = s2[windowStart];
+            windowStart += 1;
+            
+            if (prevChar in charMap) {
+                if (charMap[prevChar] === 0) {
+                    matchedChars -= 1;
+                }
+                charMap[prevChar] += 1;
+            }
+        }
     }
     
     return false;
 };
-
-const rebuildMap = (string) => {
-    let patternMap = {};
-    
-    for (let i = 0; i < string.length; i++) {
-        if (!(string[i] in patternMap)) patternMap[string[i]] = 0;
-        patternMap[string[i]] += 1;
-    }
-    
-    return patternMap;
-}
