3fc084388787a23ac29a1903c2d229fd79f5eaf2
Andy Luu <luuandyf2nd@gmail.com>
2022-09-05 21:17:43 -0700

    Time: 5457 ms (11.03%), Space: 54.7 MB (10.58%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.js
+++ 567-permutation-in-string/567-permutation-in-string.js
@@ -6,16 +6,38 @@
 var checkInclusion = function(s1, s2) {
     let windowStart = 0;
     let windowEnd = s1.length;
-    const sortedPat = s1.split('').sort().join('');
+    let patternMap = rebuildMap(s1);
     
+    console.log(patternMap)
     while(windowEnd <= s2.length) {
         let currentString = s2.substring(windowStart, windowEnd);
-        const sortedString = currentString.split('').sort().join('');
-
-        if (sortedPat === sortedString) return true;
+        
+        for (let letter of currentString) {
+            patternMap[letter] -= 1;
+            if (patternMap[letter] === 0) {
+                delete patternMap[letter];
+            }
+        }
+        
+        if (Object.keys(patternMap).length === 0) {
+            return true;
+        }
+        
         windowStart += 1;
         windowEnd += 1;
+        patternMap = rebuildMap(s1);
     }
     
     return false;
 };
+
+const rebuildMap = (string) => {
+    let patternMap = {};
+    
+    for (let i = 0; i < string.length; i++) {
+        if (!(string[i] in patternMap)) patternMap[string[i]] = 0;
+        patternMap[string[i]] += 1;
+    }
+    
+    return patternMap;
+}
