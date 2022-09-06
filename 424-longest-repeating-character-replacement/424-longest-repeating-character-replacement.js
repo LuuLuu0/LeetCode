ebc9ce1f9f852bb4cda7194d2e7cb9da8b07dc8c
Andy Luu <luuandyf2nd@gmail.com>
2022-09-05 20:23:16 -0700

    Time: 77 ms (98.48%), Space: 42.2 MB (84.89%) - LeetHub

---
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {number}
+ */
+var characterReplacement = function(s, k) {
+    let charMap = {};
+    let windowStart = 0;
+    let maxLength = 0;
+    let mostRepeatedCharCount = 0;
+    
+    for (let windowEnd = 0; windowEnd < s.length; windowEnd++) {
+        const currLetter = s[windowEnd];
+        
+        if (!(charMap[currLetter])) charMap[currLetter] = 0;
+        charMap[currLetter] += 1;
+        
+        mostRepeatedCharCount = Math.max(mostRepeatedCharCount, charMap[currLetter]);
+        
+        if (windowEnd - windowStart + 1 - mostRepeatedCharCount > k) {
+            const prevLetter = s[windowStart];
+            charMap[prevLetter] -= 1;
+            windowStart += 1;
+        }
+        maxLength = Math.max(maxLength, windowEnd - windowStart + 1);
+    }
+    return maxLength;
+};
