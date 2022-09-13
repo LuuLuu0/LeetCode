c5a9e4eeba4ca44c4f6713798dc1f8165030b1d2
Andy Luu <luuandyf2nd@gmail.com>
2022-09-13 10:25:53 -0700

    Time: 134 ms (14.94%), Space: 46 MB (83.42%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.js
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.js
@@ -10,20 +10,32 @@
  * @param {TreeNode} root
  * @return {boolean}
  */
-var isValidBST = function(root) {
-    const sortedArray = treeInorderTraversal(root);
-    let temp = sortedArray[0];
-    for (let i = 1; i < sortedArray.length; i++) {
-        if (sortedArray[i] <= temp) return false;
-        temp = sortedArray[i];
-    }
-    return true;
-};
+// var isValidBST = function(root) {
+//     const sortedArray = treeInorderTraversal(root);
+//     let temp = sortedArray[0];
+//     for (let i = 1; i < sortedArray.length; i++) {
+//         if (sortedArray[i] <= temp) return false;
+//         temp = sortedArray[i];
+//     }
+//     return true;
+// };
 
-const treeInorderTraversal = (root) => {
-    if (root === null) return [];
-    const leftPath = treeInorderTraversal(root.left);
-    const rightPath = treeInorderTraversal(root.right);
+// const treeInorderTraversal = (root) => {
+//     if (root === null) return [];
+//     const leftPath = treeInorderTraversal(root.left);
+//     const rightPath = treeInorderTraversal(root.right);
     
-    return [...leftPath, root.val, ...rightPath];
+//     return [...leftPath, root.val, ...rightPath];
+// }
+
+const isValidBST = (root) => {
+    return traverseTreeDFS(root, -Infinity, Infinity);
+}
+
+const traverseTreeDFS = (root, minBound, maxBound) => {
+    if (root === null) return true;
+    if (root.val <= minBound || root.val >= maxBound) {
+        return false;
+    }
+    return traverseTreeDFS(root.left, minBound, root.val) && traverseTreeDFS(root.right, root.val, maxBound);
 }
