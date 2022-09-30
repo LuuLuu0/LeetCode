9cba424a7e6106fb215d8835b0f2b503e9e0652c
Andy Luu <luuandyf2nd@gmail.com>
2022-09-29 18:32:40 -0700

    Time: 91 ms (90.42%), Space: 51.6 MB (95.15%) - LeetHub

--- 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.js
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.js
@@ -24,14 +24,3 @@ var lowestCommonAncestor = function(root, p, q) {
     }
 };
 
-// var lowestCommonAncestor = function(root, p, q) {
-//     while(root){
-//         if(p.val>root.val && q.val>root.val){
-//             root=root.right
-//         }else if(p.val<root.val && q.val<root.val){
-//             root=root.left
-//         }else{
-//             return root
-//         }
-//     }
-// }
