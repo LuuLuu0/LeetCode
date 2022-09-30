9df61c5d6bd42a4bdaeab80e431bf2e134800bac
Andy Luu <luuandyf2nd@gmail.com>
2022-09-29 23:11:05 -0700

    Time: 127 ms (51.70%), Space: 52.2 MB (20.90%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.js
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.js
@@ -12,10 +12,18 @@
  * @return {TreeNode}
  */
 var lowestCommonAncestor = function(root, p, q) {
-    if (root === null || root === p || root === q) return root;
+    // if the root is null or the root is equal to p or the root is equal to q
+    // we return the root
+    // otherwise we have to traverse DFS into the tree
+    // then we check if the leftPath or rightPath contains the values of P or Q
+    // once we have found it we return the root
+    if (!root || root === p || root === q) {
+        return root;
+    }
     const leftPath = lowestCommonAncestor(root.left, p, q);
     const rightPath = lowestCommonAncestor(root.right, p, q);
     if (!leftPath) return rightPath;
     if (!rightPath) return leftPath;
     return root;
+    
 };
