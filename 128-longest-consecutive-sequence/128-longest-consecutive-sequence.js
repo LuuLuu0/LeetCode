4773e62f4a97dafc751728a04320c7a928263548
Andy Luu <luuandyf2nd@gmail.com>
2022-09-30 20:39:00 -0700

    Time: 125 ms (91.42%), Space: 57.4 MB (75.60%) - LeetHub

---
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var longestConsecutive = function(nums) {
+    const numberSet = new Set();
+    
+    for (let i = 0; i < nums.length; i++) {
+        numberSet.add(nums[i]);
+    }
+    
+    let longestStreak = 0;
+    
+    for (let num of numberSet) {
+        if (!numberSet.has(num - 1)) {
+            let currentStreak = 1;
+            while (numberSet.has(num += 1)) {
+                currentStreak += 1;
+            }
+            longestStreak = Math.max(longestStreak, currentStreak);
+        }
+    }
+    return longestStreak;
+};
