d15d13d67fa0fb9d64442a8a6bc44c55e57b9be8
Andy Luu <luuandyf2nd@gmail.com>
2022-10-01 20:20:50 -0700

    Time: 96 ms (62.79%), Space: 44.5 MB (32.81%) - LeetHub

--- 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.js
+++ 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.js
@@ -11,37 +11,18 @@
  * @return {number[][]}
  */
 var levelOrder = function(root) {
-    let result = [];
+    const resultArr = [];
+    traverseDFS(root, 0);
     
-    if (root === null) return result;
-    
-    // add the root to the queue
-    // we then enqueue the node, and then iterate through the queue
-    // for each level we create a new array and push the value into that array
-    // we then check to see if that node has children, if so
-    // push the children into the queue
-    // push the array into the result array
-    
-    let queue = [];
-    queue.push(root);
-    
-    while (queue.length > 0) {
-        const currentQueueLength = queue.length;
-        let currentLevelArr = [];
-        
-        for (let i = 0; i < currentQueueLength; i++) {
-            const currentNode = queue.shift();
-            currentLevelArr.push(currentNode.val);
-            
-            if (currentNode.left !== null) {
-                queue.push(currentNode.left);
-            }
-            if (currentNode.right !== null) {
-                queue.push(currentNode.right);
-            }
+    function traverseDFS(root, levelNum) {
+        if(root === null) return;
+        if (resultArr.length === levelNum) {
+            resultArr[levelNum] = [root.val];
+        } else {
+            resultArr[levelNum].push(root.val);
         }
-        result.push(currentLevelArr);
+        traverseDFS(root.left, levelNum + 1);
+        traverseDFS(root.right, levelNum + 1);
     }
-    
-    return result
+    return resultArr;
 };
