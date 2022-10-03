9519c1aa624db0f9beaed688d42822976250c6ea
Andy Luu <luuandyf2nd@gmail.com>
2022-10-03 14:54:54 -0700

    Time: 87 ms (90.04%), Space: 44.8 MB (77.50%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.js
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.js
@@ -4,22 +4,16 @@
  * @return {number[]}
  */
 var topKFrequent = function(nums, k) {
-    // create a map to store element as well as their frequency count
-    // convert the map entry and sort it so that we can have the greatest counts in the front
-    // we can splice the array from 0 - k -1;
-    const numberCountMap = {};
-    // iterate through nums
+    let numberCountMap = {};
     for (let i = 0; i < nums.length; i++) {
-        if (!numberCountMap[nums[i]]) numberCountMap[nums[i]] = 0;
-        numberCountMap[nums[i]] += 1;
+        if (!numberCountMap[nums[i]]) numberCountMap[nums[i]] = 1;
+        else numberCountMap[nums[i]] += 1;
     }
-    const numberCountArray = Object.entries(numberCountMap)
-    numberCountArray.sort((a, b) => b[1] - a[1]);
-    const resultArr = [];
-    let counter = 0;
-    while (counter < k) {
-        resultArr.push(numberCountArray[counter][0]);
-        counter++;
+    let entries = Object.entries(numberCountMap).sort((a, b) => b[1] - a[1]);
+    
+    let resultArr = [];
+    for (let x = 0; x < k; x++) {
+        resultArr.push(entries[x][0]);
     }
     return resultArr;
 };
