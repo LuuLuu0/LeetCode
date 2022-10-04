939ea57a9ca0a3f1b2cf16f9cb7eb7952cf840a2
Andy Luu <luuandyf2nd@gmail.com>
2022-10-04 16:47:42 -0700

    Time: 160 ms (17.83%), Space: 45 MB (51.19%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.js
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.js
@@ -2,6 +2,11 @@
  * @param {number[][]} matrix
  * @return {void} Do not return anything, modify matrix in-place instead.
  */
+
+// first we have to find out all the positions that have 0s
+// im going to be using a queue to store the positions and then recursive DFS 
+// through all the directions, up/down/left/right
+
 var setZeroes = function(matrix) {
     const queue = [];
     for (let row = 0; row < matrix.length; row++) {
@@ -10,33 +15,22 @@ var setZeroes = function(matrix) {
         }
     }
     
-    // while (queue.length > 0) {
-    //     const [row, col] = queue.shift();
-    //     traverseDFS(row, col, matrix, 'up');
-    //     traverseDFS(row, col, matrix, 'left');
-    //     traverseDFS(row, col, matrix, 'down');
-    //     traverseDFS(row, col, matrix, 'right');
-    // }
-    for (let i = 0; i < queue.length; i++) {
-        let [row, col] = queue[i];
-        
-        for (let changeCol = 0; changeCol < matrix[0].length; changeCol++) {
-            matrix[row][changeCol] = 0;
-        }
-        for (let changeRow = 0; changeRow < matrix.length; changeRow++) {
-            matrix[changeRow][col] = 0;
-        }
+    while (queue.length > 0) {
+        const [row, col] = queue.shift();
+        traverseDFS(row, col, matrix, 'up');
+        traverseDFS(row, col, matrix, 'left');
+        traverseDFS(row, col, matrix, 'down');
+        traverseDFS(row, col, matrix, 'right');
     }
 };
 
-// const traverseDFS = (row, col, matrix, direction) => {
-//     const rowInbound = 0 <= row && row < matrix.length;
-//     const colInbound = 0 <= col && col < matrix[0].length;
-//     if (!rowInbound || !colInbound) return;
-//     const currPos = row + ',' + col;
-//     matrix[row][col] = 0;
-//     if (direction === 'up') traverseDFS(row + 1, col, matrix, 'up');
-//     else if (direction === 'left')  traverseDFS(row, col - 1, matrix, 'left');
-//     else if (direction === 'right')  traverseDFS(row, col + 1, matrix, 'right');
-//     else if (direction === 'down')  traverseDFS(row - 1, col, matrix, 'down');
-// }
+const traverseDFS = (row, col, matrix, direction) => {
+    const rowInbound = 0 <= row && row < matrix.length;
+    const colInbound = 0 <= col && col < matrix[0].length;
+    if (!rowInbound || !colInbound) return;
+    matrix[row][col] = 0;
+    if (direction === 'up') traverseDFS(row + 1, col, matrix, 'up');
+    else if (direction === 'left')  traverseDFS(row, col - 1, matrix, 'left');
+    else if (direction === 'right')  traverseDFS(row, col + 1, matrix, 'right');
+    else if (direction === 'down')  traverseDFS(row - 1, col, matrix, 'down');
+}
