9021ec7ca9249b7a333ff14fdc699c96d4baa865
Andy Luu <luuandyf2nd@gmail.com>
2022-10-08 16:38:32 -0700

    Time: 91 ms (83.64%), Space: 45.6 MB (45.09%) - LeetHub

--- 684-redundant-connection/684-redundant-connection.js
+++ 684-redundant-connection/684-redundant-connection.js
@@ -3,20 +3,48 @@
  * @return {number[]}
  */
 var findRedundantConnection = function(edges) {
-    // create a graph/map to store the edges 
-    let graph = {};
-    // iterate through the edges
-    for (const [nodeA, nodeB] of edges) {
-        if (find(nodeA) === find(nodeB)) {
-            return [nodeA, nodeB];
-        } else {
-            graph[find(nodeA)] = graph[find(nodeB)];
-        }
+    let map = {};
+    for (let [nodeA, nodeB] of edges){
+        if (dfs(nodeA, nodeB, new Set())) return [nodeA, nodeB]
+        
+        if (!(nodeA in map)) map[nodeA] = new Set();
+        if (!(nodeB in map)) map[nodeB] = new Set();
+        
+        map[nodeA].add(nodeB);
+        map[nodeB].add(nodeA);
     }
     
-    function find(node) {
-        if (!graph[node]) graph[node] = node;
-        if (graph[node] === node) return node;
-        return find(graph[node]);
+    function dfs(nodeA, nodeB, visited) {
+        visited.add(nodeA);
+        if (nodeA in map) {
+            if (map[nodeA].has(nodeB)) return true;
+            for (let neighbor of map[nodeA]) {
+                if (!visited.has(neighbor)) {
+                    visited.add(neighbor);
+                    if (dfs(neighbor, nodeB, visited)) return true;
+                }
+            }
+            return false;
+        }
+        return false;
     }
-}; 
+};
+
+
+//     // create a graph/map to store the edges 
+//     let graph = {};
+//     // iterate through the edges
+//     for (const [nodeA, nodeB] of edges) {
+//         if (find(nodeA) === find(nodeB)) {
+//             return [nodeA, nodeB];
+//         } else {
+//             graph[find(nodeA)] = graph[find(nodeB)];
+//             console.log(nodeA, ': ', graph)
+//         }
+//     }
+    
+//     function find(node) {
+//         if (!graph[node]) graph[node] = node;
+//         if (graph[node] === node) return node;
+//         return find(graph[node]);
+//     }
